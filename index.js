9cd1c5270c3c53a2cf00f2b52d3bf76d05304174
Utazi Fredrick <utazifredrick530@gmail.com>
2026-07-07 08:21:35 +0100

    added the QR Code  library

---
+++ index.js
@@ -0,0 +1,4 @@
+const input = document.getElementById("input-content");
+const sizeInput = document.getElementById("input-size");
+const createButton = document.getElementById("create-qr-code");
+const qrCodeContainer = document.getElementById("qr-code");
